222e4ae5018617acb6b77198906acda47bde610a
Raphael Cerqueira <raphaelfc@gmail.com>
2021-05-01 20:34:25 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Wireframe basicão de ecommerce
+Vi o [post do Gabriel Silvestri no Instagram](https://www.instagram.com/p/CONXiuYrblK/?igshid=f7156nwp14dy) com esse wireframe aí e resolvi fazer a versão dele usando SwiftUI. 
+
+## Screenshots
+<p align="center">
+    <img src="https://user-images.githubusercontent.com/16376748/116797467-4ac61080-aabc-11eb-836d-5a4a894cb62a.png" width="400"> &nbsp;
+</p>
+
+## Vídeos
+- [Vídeo no YouTube](https://youtu.be/qPApTUtWnRc)
+
+## Requisitos
+- iOS 14
+- SwiftUI
+- Xcode 12
+
+## Meta
+- Github: [https://github.com/rphlfc](https://github.com/rphlfc)
+- Instagram: [https://www.instagram.com/rphlfc.dev](https://www.instagram.com/rphlfc.dev)
+- LinkedIn: [http://www.linkedin.com/in/rphlfc](http://www.linkedin.com/in/rphlfc)
+- Twitter: [https://twitter.com/rphlfc](https://twitter.com/rphlfc)
+- Website: [https://rphlfc.github.io](https://rphlfc.github.io)
+- Youtube: [https://www.youtube.com/c/raphaelcerqueira](https://www.youtube.com/c/raphaelcerqueira)
+
--- Wireframe/ContentView.swift
+++ Wireframe/ContentView.swift
@@ -8,9 +8,208 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var search = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color("BackgroundColor")
+                .ignoresSafeArea()
+            
+            ScrollView(.vertical, showsIndicators: false, content: {
+                VStack {
+                    HStack {
+                        HStack {
+                            Image(systemName: "magnifyingglass")
+                                .font(.title)
+                                .foregroundColor(Color(#colorLiteral(red: 0.3411838412, green: 0.1000181958, blue: 0.6608016491, alpha: 1)))
+                                
+                            TextField("", text: $search)
+                            
+                            Spacer()
+                        }
+                        .padding(8)
+                        .background(
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color("ForegroundColor"))
+                                .border(Color(#colorLiteral(red: 0.3411838412, green: 0.1000181958, blue: 0.6608016491, alpha: 1)), width: 2)
+                        )
+                        
+                        
+                        Button(action: {}, label: {
+                            ZStack(alignment: .topTrailing) {
+                                Image(systemName: "cart")
+                                    .font(.title)
+                                    .foregroundColor(Color("ForegroundColor"))
+                                
+                                Circle()
+                                    .frame(width: 12, height: 12)
+                                    .foregroundColor(Color(#colorLiteral(red: 0.1959293485, green: 0.05765388161, blue: 0.3307349086, alpha: 1)))
+                            }
+                        })
+                    }
+                    .padding(.horizontal)
+                    
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("Lorem ipsum dolor sit amet")
+                                .redacted(reason: .placeholder)
+                            
+                            Text("Lorem ipsum")
+                                .redacted(reason: .placeholder)
+                        }
+                        
+                        Spacer()
+                    }
+                    .padding(.top)
+                    .padding(.horizontal)
+                    
+                    Button(action: {}, label: {
+                        
+                    })
+                    .frame(width: 130, height: 30)
+                    .background(Color("AccentColor"))
+                    .cornerRadius(5)
+                    .padding(.top, 20)
+                    
+                    LazyVGrid(columns: Array.init(repeating: GridItem(.adaptive(minimum: 200, maximum: 200)), count: 4), content: {
+                        ForEach(0..<8) { index in
+                            VStack {
+                                Circle()
+                                    .frame(width: 40, height: 40)
+                                    .foregroundColor(Color("BackgroundColor"))
+                                    .redacted(reason: .placeholder)
+                                
+                                Text("Lorem")
+                                    .redacted(reason: .placeholder)
+                            }
+                        }
+                    })
+                    .padding()
+                    .background(Color("ForegroundColor"))
+                    .padding(.top)
+                    
+                    VStack {
+                        HStack {
+                            Text("Lorem ipsum dolor sit amet")
+                                .redacted(reason: .placeholder)
+                            
+                            Spacer()
+                            
+                            Button(action: {}, label: {
+                                Text("Lorem ipsum")
+                                    .foregroundColor(Color("AccentColor"))
+                                    .redacted(reason: .placeholder)
+                                
+                                Image(systemName: "chevron.right")
+                                    .foregroundColor(Color("AccentColor"))
+                            })
+                        }
+                        .padding(.horizontal)
+                    
+                        HStack {
+                            Image(systemName: "photo")
+                                .resizable()
+                                .frame(width: 80, height: 80)
+                                .redacted(reason: .placeholder)
+                            
+                            VStack(alignment: .leading) {
+                                Text("Lorem ipsum")
+                                    .redacted(reason: .placeholder)
+                                
+                                Text("Lorem")
+                                    .redacted(reason: .placeholder)
+                                
+                                Spacer()
+                                
+                            }
+                            
+                            Spacer()
+                            
+                            Button(action: {}, label: {
+                                
+                            })
+                            .frame(width: 130, height: 30)
+                            .background(Color("AccentColor"))
+                            .cornerRadius(5)
+                            .padding(.top, 20)
+                        }
+                        .frame(height: 80)
+                        .padding(.horizontal)
+                        
+                    }
+                    .padding(.top)
+                    
+                    VStack {
+                        HStack {
+                            Text("Lorem ipsum")
+                                .redacted(reason: .placeholder)
+                            
+                            Spacer()
+                        }
+                        .padding(.top)
+                        .padding(.horizontal)
+                        
+                        ScrollView(.horizontal, showsIndicators: false, content: {
+                            HStack(spacing: 15) {
+                                ForEach(0..<8) { index in
+                                    VStack {
+                                        Image(systemName: "photo")
+                                            .resizable()
+                                            .frame(width: 70, height: 70)
+                                            .redacted(reason: .placeholder)
+                                        
+                                        HStack {
+                                            VStack(alignment: .leading) {
+                                                Text("Lorem ipsum")
+                                                    .font(.caption)
+                                                    .redacted(reason: .placeholder)
+                                                
+                                                Text("Lorem")
+                                                    .font(.caption2)
+                                                    .redacted(reason: .placeholder)
+                                            }
+                                            
+                                            Spacer(minLength: 0)
+                                        }
+                                    }
+                                    .padding()
+                                    .background(Color("BackgroundColor"))
+                                }
+                                
+                            }
+                            .padding(.horizontal)
+                            .padding(.bottom)
+                        })
+                    }
+                    .background(Color("ForegroundColor"))
+                    .padding(.top)
+                    
+                    Spacer(minLength: 0)
+                }
+            })
+        
+            VStack {
+                Spacer()
+                
+                HStack(spacing: 30) {
+                    ForEach(0..<5) { index in
+                        Button(action: {}, label: {
+                            VStack(spacing: 8) {
+                                Circle()
+                                    .frame(width: 30, height: 30)
+                                    
+                                Text("Botão")
+                                    .font(.caption)
+                            }
+                            .foregroundColor(Color("ForegroundColor"))
+                        })
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color("BackgroundColor").ignoresSafeArea(.all, edges: .bottom))
+            }
+        }
     }
 }
 
