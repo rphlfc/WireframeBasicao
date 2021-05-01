ce6e03abd28b31cc258cec79367fa423f1b06ac0
Raphael Cerqueira <raphaelfc@gmail.com>
2021-05-01 19:18:49 -0300

    Initial Commit

---
+++ Wireframe/WireframeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WireframeApp.swift
+//  Wireframe
+//
+//  Created by Raphael Cerqueira on 01/05/21.
+//
+
+import SwiftUI
+
+@main
+struct WireframeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
